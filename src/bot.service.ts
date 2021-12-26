fe94cb18ab72ee1ca1e783ea4b6ca4c9053753a2
Tuan Le Thanh <tuanitpro@gmail.com>
2021-12-26 19:12:29 +0700

    Update

--- src/bot.service.ts
+++ src/bot.service.ts
@@ -1,5 +1,7 @@
 import { Injectable, Logger, OnApplicationBootstrap } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
+
+import { pricePairs } from './pricePairs';
 const TelegramBot = require('node-telegram-bot-api');
 const Binance = require('node-binance-api');
 
@@ -22,34 +24,67 @@ export class BotService implements OnApplicationBootstrap {
 
       bot.sendMessage(chatId, 'Hello Tuan');
     });
-
+    bot.onText(/\/balance/, async (msg, match) => {
+      await binance.balance((error, balances) => {
+        if (error) return console.error(error);
+        let response: string = '';
+        pricePairs.forEach((item) => {
+          if (balances[item].available > 0.0) {
+            response += '\n' + item + ' balance: ' + balances[item].available;
+          }
+        });
+        const chatId = msg.chat.id;
+        bot.sendMessage(chatId, response);
+      });
+    });
     bot.onText(/\/price (.+)/, async (msg, match) => {
       const chatId = msg.chat.id;
       const resp = match[1];
       const symbol = resp.toUpperCase();
       let ticker = await binance.prices();
-
       let response = '';
       switch (symbol) {
         case 'BTC':
-          response = `Price of ${symbol}: ${ticker.BTCUSDT}`;
+        case 'USDT':
+          response = `${symbol}_USDT: ${ticker.BTCUSDT}`;
           bot.sendMessage(chatId, response);
           break;
         case 'BNB':
-          response = `Price of ${symbol}: ${ticker.BNBUSDT}`;
-          bot.sendMessage(chatId, response);
-          break;
-        case 'OMG':
-          response = `Price of ${symbol}: ${ticker.OMGUSDT}`;
+          response = `${symbol}_USDT: ${ticker.BNBUSDT}`;
+          response += `\n${symbol}_BTC: ${ticker.BNBBTC}`;
           bot.sendMessage(chatId, response);
           break;
         case '24H':
-          await binance.prevDay('BNBBTC', (error, prevDay, symbol) => {
+          await binance.prevDay('BTCUSDT', (error, prevDay, symbol) => {
             console.info(symbol + ' previous day:', prevDay);
-            response =
-              'BNB change since yesterday: ' + prevDay.priceChangePercent + '%';
+            response = `BTC change since yesterday: 
+            Last: ${prevDay.lastPrice}  
+            High: ${prevDay.highPrice}
+            Low : ${prevDay.lowPrice}
+            ${prevDay.priceChangePercent} %`;
             bot.sendMessage(chatId, response);
           });
+          break;
+        default:
+          const getSymbol = pricePairs.find((item) => item === symbol);
+          if (getSymbol) {
+            const getTickerUSDT = symbol + 'USDT';
+            const getTickerBTC = symbol + 'BTC';
+            const getTickerBNB = symbol + 'BNB';
+            if (ticker[getTickerUSDT]) {
+              response = ` ${symbol}_USDT: ${ticker[getTickerUSDT]}`;
+            }
+            if (ticker[getTickerBTC]) {
+              response += `\n${symbol}_BTC: ${ticker[getTickerBTC]}`;
+            }
+            if (ticker[getTickerBNB]) {
+              response += `\n${symbol}_BNB: ${ticker[getTickerBNB]}`;
+            }
+            bot.sendMessage(chatId, response);
+          } else {
+            bot.sendMessage(chatId, symbol + ' not found');
+          }
+
           break;
       }
     });
