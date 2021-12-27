527aa13206afcae23c2d463dcb87d95cbeaf75fd
Tuan Le Thanh <tuanitpro@gmail.com>
2021-12-28 00:32:04 +0700

    Add cache

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
-import { Module, NestModule, MiddlewareConsumer } from '@nestjs/common';
+import { Module, NestModule, MiddlewareConsumer, CacheModule } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { ScheduleModule } from '@nestjs/schedule';
 import { AppService } from './app.service';
 import configuration from './configuration';
 import { BotService } from './bot.service';
+import * as redisStore from 'cache-manager-redis-store';
 
 @Module({
   imports: [
@@ -12,6 +13,19 @@ import { BotService } from './bot.service';
       load: [configuration],
     }),
     ScheduleModule.forRoot(),
+    CacheModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      isGlobal: true,
+      useFactory: async (configService: ConfigService) => ({
+          store: redisStore,
+          host: configService.get<string>('redis.host'),
+          port: configService.get<number>('redis.port'),
+          auth_pass: configService.get<string>('redis.password'),
+          no_ready_check: true,
+          ttl: 3600,
+      }),
+  }),
   ],
   controllers: [],
   providers: [
--- src/bot.service.ts
+++ src/bot.service.ts
@@ -1,4 +1,6 @@
 import {
+  CACHE_MANAGER,
+  Inject,
   Injectable,
   Logger,
   OnApplicationBootstrap,
@@ -7,7 +9,8 @@ import {
 import { ConfigService } from "@nestjs/config";
 import { Cron, CronExpression } from "@nestjs/schedule";
 import { SentryInterceptor } from "./interceptors/sentry.interceptor";
-
+import { Cache } from "cache-manager";
+import { RSI, MACD, SMA, MovingAverage } from "trading-signals";
 import { pricePairs } from "./pricePairs";
 const TelegramBot = require("node-telegram-bot-api");
 const Binance = require("node-binance-api");
@@ -19,39 +22,53 @@ export class BotService implements OnApplicationBootstrap {
 
   private token: string = "";
   private bot;
-  constructor(private readonly configService: ConfigService) {
+  constructor(
+    private readonly configService: ConfigService,
+    @Inject(CACHE_MANAGER) private cacheManager: Cache
+  ) {
     this.token = this.configService.get<string>("telegram.token");
     this.bot = new TelegramBot(this.token, { polling: true });
   }
 
   @Cron(CronExpression.EVERY_30_SECONDS)
   async handleCron() {
-    const chatId = this.configService.get<string>("telegram.chatId");
-    const binance = new Binance().options({
-      APIKEY: this.configService.get<string>("binance.apiKey"),
-      APISECRET: this.configService.get<string>("binance.apiSecret"),
-    });
-    const prevDays = await binance.prevDay(false);
-    await binance.balance((error, balances) => {
-      if (error) return this.logger.error(error);
-      let response: string = "";
-      pricePairs.forEach((item) => {
-        if (balances[item].available > 0.0) {
-          const findPrevDay = prevDays.find((x) => x.symbol === item + "USDT");
-          if (findPrevDay) {
-            if (
-              findPrevDay.priceChangePercent >= 10.0 ||
-              findPrevDay.priceChangePercent <= -4.999
-            ) {
-              response += `${item} has change to ${findPrevDay.lastPrice} ${findPrevDay.priceChangePercent} %`;
+    const command: string =
+      (await this.cacheManager.get("JOB_COMMAND")) || "START";
+    if (command === "START") {
+      const chatId = this.configService.get<string>("telegram.chatId");
+      const binance = new Binance().options({
+        APIKEY: this.configService.get<string>("binance.apiKey"),
+        APISECRET: this.configService.get<string>("binance.apiSecret"),
+      });
+      const minPrice: number =
+        (await this.cacheManager.get("MIN_PRICE")) || -4.9999;
+      const maxPrice: number = (await this.cacheManager.get("MAX_PRICE")) || 10;
+      const prevDays = await binance.prevDay(false);
+      await binance.balance((error, balances) => {
+        if (error) return this.logger.error(error);
+        let response: string = "";
+        pricePairs.forEach((item) => {
+          if (balances[item].available > 0.0) {
+            const findPrevDay = prevDays.find(
+              (x) => x.symbol === item + "USDT"
+            );
+            if (findPrevDay) {
+              if (
+                findPrevDay.priceChangePercent <=
+                  Number.parseFloat(minPrice.toString()) ||
+                findPrevDay.priceChangePercent >=
+                  Number.parseFloat(maxPrice.toString())
+              ) {
+                response += `${item} has change to ${findPrevDay.lastPrice} ${findPrevDay.priceChangePercent} %\n`;
+              }
             }
           }
+        });
+        if (response) {
+          this.bot.sendMessage(chatId, response);
         }
       });
-      if (response) {
-        this.bot.sendMessage(chatId, response);
-      }
-    });
+    }
   }
 
   onApplicationBootstrap() {
@@ -61,10 +78,60 @@ export class BotService implements OnApplicationBootstrap {
       APISECRET: this.configService.get<string>("binance.apiSecret"),
     });
 
-    this.bot.onText(/\/start/, (msg, match) => {
+    this.bot.onText(/\/start/, async (msg, match) => {
       const chatId = msg.chat.id;
+      await this.cacheManager.set("JOB_COMMAND", "START");
+      this.bot.sendMessage(chatId, "Start watching...");
+    });
+    this.bot.onText(/\/stop/, async (msg, match) => {
+      const chatId = msg.chat.id;
+      await this.cacheManager.set("JOB_COMMAND", "STOP");
+      this.bot.sendMessage(chatId, "Stop watching...");
+    });
+    this.bot.onText(/\/watch (.+) (.+)/, async (msg, match) => {
+      const chatId = msg.chat.id;
+      await this.cacheManager.set("MIN_PRICE", Number.parseFloat(match[1]));
+      await this.cacheManager.set("MAX_PRICE", Number.parseFloat(match[2]));
+      this.bot.sendMessage(
+        chatId,
+        `Watching... MIN_PRICE: ${match[1]} MAX_PRICE: ${match[2]}`
+      );
+    });
+    this.bot.onText(/\/rsi (.+)/, async (msg, match) => {
+      const chatId = msg.chat.id;
+      const timing: string = match[1];
+      binance.candlesticks(
+        "LTCUSDT",
+        timing,
+        (error, ticks, symbol) => {
+          if (error) return this.logger.error(error);
+          const rsi = new RSI(6);
+          ticks.forEach((element) => {
+            let [
+              time,
+              open,
+              high,
+              low,
+              close,
+              volume,
+              closeTime,
+              assetVolume,
+              trades,
+              buyBaseVolume,
+              buyAssetVolume,
+              ignored,
+            ] = element;
+            rsi.update(open);
+            rsi.update(close);
+          });
 
-      this.bot.sendMessage(chatId, "Hello Tuan");
+          this.bot.sendMessage(
+            chatId,
+            `RSI ${timing.toUpperCase()} ${rsi.getResult().valueOf().toString()}`
+          );
+        },
+        { limit: 500, endTime: new Date().getTime() }
+      );
     });
     this.bot.onText(/\/balance/, async (msg, match) => {
       await binance.balance((error, balances) => {
@@ -79,6 +146,7 @@ export class BotService implements OnApplicationBootstrap {
         this.bot.sendMessage(chatId, response);
       });
     });
+
     this.bot.onText(/\/price (.+)/, async (msg, match) => {
       const chatId = msg.chat.id;
       const resp = match[1];
@@ -90,7 +158,6 @@ export class BotService implements OnApplicationBootstrap {
         case "BTC":
         case "USDT":
           const findPrevDay = prevDays.find((x) => x.symbol === "BTCUSDT");
-
           response = `${symbol}_USDT: ${ticker.BTCUSDT}`;
           response += "\n" + this.getResponsePrevDay("", findPrevDay);
           this.bot.sendMessage(chatId, response);
