6a567f6899faaeea7c18b1059d8115ea2b70536d
Tuan Le Thanh <tuanitpro@gmail.com>
2021-12-27 11:10:10 +0700

    update logger

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,14 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
-import { AppService } from './app.service';
- 
+import { NestFactory } from "@nestjs/core";
+import { AppModule } from "./app.module";
+import { AppService } from "./app.service";
+
 async function bootstrap() {
-  const appContext = await NestFactory.createApplicationContext(AppModule);
+  const appContext = await NestFactory.createApplicationContext(AppModule, {
+    logger:
+      process.env.ENV === "production"
+        ? ["warn", "error"]
+        : ["debug", "log", "verbose"],
+  });
   const appService = appContext.get(AppService);
   console.log(appService.getHello());
 }
